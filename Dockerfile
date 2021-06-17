653cc647b16c777f5a9bb924b3f8b46ce927d175
tweek <vallkhale@gmail.com>
2021-06-17 20:18:52 +0300

    initial

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:8-jdk
+EXPOSE 8080:8080
+RUN mkdir /app
+COPY ./build/install/ktor-demo/ /app/
+WORKDIR /app/bin
+
+CMD ["./ktor-demo"]
---
+++ src/Application.kt
@@ -0,0 +1,57 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.sessions.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+
+        get("/session/increment") {
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+    }
+}
+
+data class MySession(val count: Int = 0)
+
