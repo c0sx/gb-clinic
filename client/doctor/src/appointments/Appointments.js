a572e64d21bcc3773d5a120b947a6a9b463278f1
tweek <vallkhale@gmail.com>
2021-07-11 23:55:20 +0300

    doctor cabinet

--- client/doctor/src/appointments/Appointments.js
+++ client/doctor/src/appointments/Appointments.js
@@ -3,10 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { getAppointmentsAsync } from "../saga/getAppointmentsAsync";
 import { selectAppointments } from "../store/appointments/selectors";
-import {selectDoctorId} from "../store/doctor/selectors";
-import {getDoctorInfoAsync} from "../saga/getDoctorInfoAsync";
-import {signOut} from "../api/signout";
-
+import { selectDoctorId } from "../store/doctor/selectors";
+import { getDoctorInfoAsync } from "../saga/getDoctorInfoAsync";
+import { signOut } from "../api/signout";
 
 export const Appointments = () => {
     const dispatch = useDispatch();
